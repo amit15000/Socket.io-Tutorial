cad56c768c19e84fd87cbf08f384b2eca172b533
Amit Kumar <15000amitkumar@gmail.com>
2024-01-31 21:55:26 +0530

    Connection Established

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 
-const PORT = 3000;
+const PORT = 5000;
 const app = express();
-const server = createServer(app);
-const io = new Server(server);
 
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 app.get("/", (req, res) => {
   res.send("Aayein");
 });
@@ -16,7 +23,7 @@ io.on("connection", (socket) => {
   console.log("Id :", socket.id);
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   {
     console.log(`Server is running on Port ${PORT}`);
   }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
+import { io } from "socket.io-client";
 
 function App() {
+  const socket = io("http://localhost:5000");
+
   return <div className="App"></div>;
 }
 
