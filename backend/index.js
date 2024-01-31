bddc5e2902b168b14c01aa202f78630ae0cf067f
Amit Kumar <15000amitkumar@gmail.com>
2024-01-31 19:14:33 +0530

    Connection Setup

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,22 @@
-const express = require("express");
-const dbConnect = require("./Config/database");
-dbConnect();
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const PORT = 3000;
 const app = express();
-require("dotenv").config();
-PORT = process.env.PORT || 3000;
+const server = createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.send("Aayein");
+});
+
+io.on("connection", (socket) => {
+  console.log("User Conencted");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server Running Successfuly on ${PORT}`);
+  {
+    console.log(`Server is running on Port ${PORT}`);
+  }
 });
